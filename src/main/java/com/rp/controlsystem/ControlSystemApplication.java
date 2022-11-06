c15e9f2dff545e06f1db0831cac20a42eeddfe20
Carolina <carolinaruo@gmail.com>
2022-11-06 19:44:22 -0300

    Projeto base

---
+++ src/main/java/com/rp/controlsystem/ControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rp.controlsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlSystemApplication.class, args);
+	}
+
+}
